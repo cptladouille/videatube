48977e454019425c0098aef74fbeaab3374bb674
ThoninChloe <chloe.thonin@gmail.com>
2018-12-19 15:03:38 +0100

    Ajout page connexion + inscription

--- view/connexion.php
+++ view/connexion.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chloethonin
- * Date: 18/12/2018
- * Time: 14:08
- */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CONNECTEZ VOUS</title>
+</head>
+<body>
+<h1>CONNECTEZ VOUS !</h1>
+<form method="post" action="action.php">
+
+
+    <label for="Login">Login :</label>
+    <input type="text" name="login" placeholder=" Choisissez un Login"/>
+    <br>
+    <br>
+
+    <label for="MotDePasse">Mot de Passe :</label>
+    <input type="password" name="mdp" placeholder="Choisissez un mdp"/>
+    <br>
+    <br>
+
+    <input type="submit" value="Se connecter" />
+
+</form>
+</body>
+
+</html>
--- view/template.php
+++ view/template.php
@@ -58,7 +58,7 @@
 </head>
 
 <body>
-<? = $content ?>
+<? $content ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
