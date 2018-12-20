11cf286b6ccaa6f7b3c338513ae465e27043061b
ThoninChloe <chloe.thonin@gmail.com>
2018-12-20 14:02:24 +0100

    CSS page connexion

--- view/connexion.php
+++ view/connexion.php
@@ -1,29 +1,35 @@
 <?php $title = 'Connexion';
 ob_start(); ?>
-    <div class="col-sm-12 container">
-            <div class="formulaireTitle">
-                <h1>CONNECTEZ VOUS !</h1>
-                <div class="col-sm-offset-4 col-sm-4">
-                    <form method="post" action="action.php">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 formulaire">
+            <h1>CONNECTEZ VOUS !</h1>
+            <form method="post" action="action.php">
+                <div class="formulaireChamps">
+                    <label for="Login">Login :</label>
+                </div>
+                <div>
+                    <input type="text" class="form-control formulaireInput" name="login" placeholder="Login"/>
+                </div>
+                <br>
+                <div class="formulaireChamps">
+                    <label for="MotDePasse">Mot de Passe :</label>
+                </div>
+                <div>
+                    <input type="password" class="form-control formulaireInput" name="mdp" placeholder="Mot de passe"/>
+                </div>
+                <br>
+                <div>
+                    <button type="button" class="btn" value="Se connecter">Se connecter</button>
+                </div>
+            </form>
 
-                        <div class="col-sm-offset-1 col-sm-3 ">
-                        <label for="Login">Login :</label>
-                        <input type="text" name="login" placeholder="Entrez votre login"/>
-                        <br>
-                        <br>
-                        </div>
-
-                        <div class="col-sm-offset-1 col-sm-3 ">
-                        <label for="MotDePasse">Mot de Passe :</label>
-                        <input type="password" name="mdp" placeholder="Entrez votre mdp"/>
-                        <br>
-                        <br>
-                        </div>
-                        <button type="button" class="btn btn-secondary" value="Se connecter">Se connecter</button>
-                    </form>
-                    </div>
-            </div>
+        </div>
     </div>
+
+</div>
+
+
 <?php $content = ob_get_clean();
-require_once ('view/template.php');?>
+require_once('view/template.php'); ?>
 
