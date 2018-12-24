8f60fa33cf839971cf3b0fd98c5d72c56b74f6d3
ThoninChloe <chloe.thonin@gmail.com>
2018-12-24 10:32:15 +0100

    c bo

--- view/template.php
+++ view/template.php
@@ -47,10 +47,10 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" formaction='connexion'>Connexion</button>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" formaction='profil'>Profil</button>
+                <input class="form-control mr-sm-2 inputHome" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn  my-2 my-sm-0 BoutonHome" type="submit">Search</button>
+                <button class="btn  my-2 my-sm-0 BoutonHome" type="submit" formaction='connexion'>Connexion</button>
+                <button class="btn  my-2 my-sm-0 BoutonHome" type="submit" formaction='profil'>Profil</button>
 
             </form>
         </div>
