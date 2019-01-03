ad9ad44d8933c0e3cc8cd3f88993e22e35165d20
ThoninChloe <chloe.thonin@gmail.com>
2019-01-03 23:00:30 +0100

    fyghioj

--- view/profil.php
+++ view/profil.php
@@ -1,7 +1,15 @@
 <?php $title = 'Profil';
 ob_start(); ?>
 
+<div class="containerProfil col-lg-10">
+    <div class="photoProfil">
+        <img src=http://image.noelshack.com/fichiers/2019/01/4/1546507376-profile-2092113-12802.png >
+    </div>
+    <div class="ChampProfil">
+        <p><?= $data[0] ?></p>
+    </div>
 
+</div>
 
 
 
