c1433a23a6fd6b1089a9a7bf1b38c13b4f3c52b4
ThoninChloe <chloe.thonin@gmail.com>
2019-01-07 15:49:50 +0100

    Ajout Carousel

--- view/home.php
+++ view/home.php
@@ -1,63 +1,284 @@
 <?php $title = 'Acceuil';
 ob_start(); ?>
 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+    </head>
+    <body>
 
-
-
-
-
-
-
-
-
-
-
-
-    <div class='containerhome'>
-        <div class='row'>
-            <div>Vidéos Récentes gratuites :
-                <br>
-                <div class='row ligneVideo'>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[0] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[1] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[2] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[3] ?>"></object>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container">
         <br>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
 
-        <div class='row'>
-            <div>Vidéos Récentes monétisées :
-                <div class='row ligneVideo'>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[0] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[1] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[2] ?>"></object>
-                    </div>
-                    <div class='col-lg-3 video'>
-                        <object data="<?= $data[3] ?>"></object>
-                    </div>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+
+                <div class="item active">
+                    <table class="tableauCarousel">
+                        <tr>
+                            <td>
+                                <div class="colonneTab firstColTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the bulk
+                                                    of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab ">
+
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </table>
+
                 </div>
-            </div>
-        </div>
 
+                <div class="item">
+                    <table class="tableauCarousel">
+                        <tr>
+                            <td>
+                                <div class="colonneTab firstColTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the bulk
+                                                    of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab ">
+
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </table>
+
+                </div>
+
+                <div class="item">
+                    <table class="tableauCarousel">
+                        <tr>
+                            <td>
+                                <div class="colonneTab firstColTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the bulk
+                                                    of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+                                            <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab ">
+
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+
+                                        <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </table>
+
+                </div>
+
+                <div class="item">
+                    <table class="tableauCarousel">
+                        <tr>
+                            <td>
+                                <div class="colonneTab firstColTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+
+                                        <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the bulk
+                                                    of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab">
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+
+                                        <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="colonneTab ">
+
+                                    <div class="card mb-2">
+                                        <object data="<?= $data[0] ?>"></object>
+
+                                        <div class="card-body">
+                                                <h4 class="card-title">Card title</h4>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and
+                                                    make up the
+                                                    bulk of the
+                                                    card's content.</p>
+                                                <a class="btn btn-primary btnCarou">Button</a>
+                                            </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </table>
+
+                </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
-    <br>
-    <br>
+
+    </body>
+    </html>
 
 
 <?php $content = ob_get_clean();
