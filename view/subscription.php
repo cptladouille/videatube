16e4840e8b14af777364fd7fdfeb234645b08b5e
ThoninChloe <chloe.thonin@gmail.com>
2019-01-16 00:01:54 +0100

    background Subscription

--- view/subscription.php
+++ view/subscription.php
@@ -7,7 +7,7 @@ ob_start(); ?>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
-
+<body class="bodySub">
    <!------ Include the above in your HEAD tag ---------->
     <div class="container-fluid bg-gradient p-5 containerAbo">
         <div class="row m-auto text-center w-75">
@@ -92,7 +92,7 @@ ob_start(); ?>
 
         </div>
     </div>
-
+</body>
 
 
 <?php $content = ob_get_clean();
