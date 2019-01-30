0dfdf4f294b3e569538f33578607db07b43defa9
ThoninChloe <chloe.thonin@gmail.com>
2019-01-30 16:38:59 +0100

    conditon

--- view/condition.php
+++ view/condition.php
@@ -96,10 +96,7 @@ ob_start(); ?>
                         augue.</p>
                 </div><!-- /.blog-post -->
 
-                <nav class="blog-pagination">
-                    <a class="btn btn-outline-primary" href="#">Older</a>
-                    <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-                </nav>
+               
 
             </div><!-- /.blog-main -->
 
