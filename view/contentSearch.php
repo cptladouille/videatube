4a249853e1123df06afdf6704d30885067fff58f
ThoninChloe <chloe.thonin@gmail.com>
2019-02-22 00:19:02 +0100

    correctif

--- view/contentSearch.php
+++ view/contentSearch.php
@@ -3,20 +3,20 @@
     if(isset($videosFound))
     {
         if(count($videosFound)>0)
-        { 
+        {
             for($i = 1;$i<count($videosFound);$i++)
             { ?>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
-                    <?php if($videosFound[$i]->getThumbnail() == "")
-                    { ?>
-                        <a href="video-<?= $videosFound[$i]->getId(); ?>"><img class="card-img-top" width="300" height="210" src="./ressources/default.svg" alt=""></a>
-                    <?php 
-                    }else
-                    { ?>
-                    <a href="video-<?= $videosFound[$i]->getId(); ?>"><img class="card-img-top" width="300" height="210" src="./ressources/thumb/<?= $videosFound[$i]->getThumbnail(); ?>" alt=""></a>
-                    <?php 
-                    } ?>
+                        <?php if($videosFound[$i]->getThumbnail() == "")
+                        { ?>
+                            <a href="video-<?= $videosFound[$i]->getId(); ?>"><img class="card-img-top" width="300" height="210" src="./ressources/default.svg" alt=""></a>
+                            <?php
+                        }else
+                        { ?>
+                            <a href="video-<?= $videosFound[$i]->getId(); ?>"><img class="card-img-top" width="300" height="210" src="./ressources/thumb/<?= $videosFound[$i]->getThumbnail(); ?>" alt=""></a>
+                            <?php
+                        } ?>
                         <div class="card-body">
                             <h4 class="card-title">
                                 <a href="video-<?= $videosFound[$i]->getId(); ?>" class="titreVideo"><?= $videosFound[$i]->getTitle(); ?></a>
@@ -33,15 +33,14 @@
                         </div>
                     </div>
                 </div>
-            <?php 
+                <?php
             }
-        } 
-        else{ ?>  
-            <p class="VideoNonTrouve">Aucune vidéo trouvée pour l'argument de recherche : <?= $_POST['search'] ?></p>
+        }
+        else{ ?>
+            <h1>Aucune vidéo trouvée pour l'argument de recherche : <?= $_POST['search'] ?></h1>
         <?php }
     }else
     { ?>
         <h1>Aucune vidéo trouvée</h1>
     <?php } ?>
-    </div>
 </div>
--- view/sideSearch.php
+++ view/sideSearch.php
@@ -8,18 +8,18 @@
                         <div class="holderCircle">
                             <div class="round"></div>
                             <div class="dotCircle">
-                                <?php 
+                                <?php
                                 for($ti = 0; $ti<count($dataThemes);$ti++)
                                 { ?>
                                     <span class="itemDot itemDot<?= $ti ?>" data-tab="<?= $ti ?>">
                                         <i class="<?= $dataThemes[$ti]->getThumbnail(); ?>"></i>
                                         <span class="forActive"></span>
                                     </span>
-                                <?php 
+                                    <?php
                                 }  ?>
                             </div>
                             <div class="contentCircle">
-                                <?php  
+                                <?php
                                 for($ti = 0; $ti<count($dataThemes);$ti++)
                                 { ?>
                                     <div class="CirItem title-box CirItem<?= $ti ?>">
@@ -27,7 +27,7 @@
                                         <p><?= $dataThemes[$ti]->getDescription(); ?></p>
                                         <i class="<?= $dataThemes[$ti]->getThumbnail(); ?>"></i>
                                     </div>
-                                <?php 
+                                    <?php
                                 } ?>
                             </div>
                         </div>
@@ -37,7 +37,7 @@
         </section>
     </div>
     <div class="row">
-        <form class="searchSide" method = 'post' action ='search'>  
+        <form class="searchSide" method = 'post' action ='search'>
             <div class="search__wrapper">
                 <input type="text" name="search" placeholder="Tapez votre recherche..." class="search__field">
                 <button type="submit" class="fa fa-search search__icon"></button>
@@ -45,9 +45,9 @@
         </form>
     </div>
     <div class="row">
-    <div class="slidecontainer">
-  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-</div>
+        <div class="slidecontainer">
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        </div>
     </div>
 </div>
 
