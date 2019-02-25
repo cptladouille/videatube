f63ce0395f569208ee3fa39c79c25f155e5d4018
Unknown <remymouttet67@gmail.com>
2019-02-25 21:16:36 +0100

    ajout menu sidesearch

--- view/sideSearch.php
+++ view/sideSearch.php
@@ -1,7 +1,15 @@
 <div class="sideSearchContainer">
     <form>
-        
-    
+        <div class="list-group">
+            <a href="theme-1" class="list-group-item">Action</a>
+            <a href="theme-2" class="list-group-item">Aventure</a>
+            <a href="theme-3" class="list-group-item">Cuisine</a>
+            <a href="theme-4" class="list-group-item">Beauté</a>
+            <a href="theme-5" class="list-group-item">Animaux</a>
+            <a href="theme-6" class="list-group-item">Tuto</a>
+            <a href="theme-0" class="list-group-item">Voir tous les thèmes</a>
+        </div>
+        <br>
         <div class= "form-check-container">
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="radio" id="R1" value="option1" checked>
